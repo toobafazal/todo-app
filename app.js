15114f929c3881ba5c1f729d69cb6fc470729cde
tooba fazal <toobafazal75@gmail.com>
2021-08-08 11:49:37 -0700

    to do app complete

--- app.js
+++ app.js
@@ -5,22 +5,47 @@ function addTodo(){
     // create li with text node
     var li = document.createElement('li')
     var liText = document.createTextNode(todo_item.value);
-    li.appendChild(liText)
+
+if(todo_item.value == ""){
+    alert("enter any todo");
+}
+else{
 
 
 // create delete button 
 var delbtn = document.createElement('button')
-var delText = document.createTextNode("delete")
-delbtn.appendChild(delText)
+var delText = document.createTextNode("DELETE")
 delbtn.setAttribute("class","btn")
 delbtn.setAttribute("onClick","deleteItem(this)")
 
+// create edit button 
+var editbtn = document.createElement('button')
+var editText = document.createTextNode("EDIT")
+editbtn.setAttribute("class","btn")
+editbtn.setAttribute("onClick","editItem(this)")
+
+
+
+
+    delbtn.appendChild(delText)
+    editbtn.appendChild(editText)
+    li.appendChild(liText)
+    li.appendChild(editbtn)
     li.appendChild(delbtn)
     list.appendChild(li)
     todo_item.value = " ";
     console.log(li)
 }
-function deleteItem(e){
+}
+function deleteItem(f){
     
-    e.parentNode.remove()
+    f.parentNode.remove()
+}
+function deleteAll(){
+    list.innerHTML = ""
+}
+function editItem(e){
+    var change = e.parentNode.firstChild.nodeValue;
+    var editvalue = prompt("Edit Todo",change)
+    e.parentNode.firstChild.nodeValue = editvalue
 }
